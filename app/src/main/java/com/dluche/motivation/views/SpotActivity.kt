444dca71d016bac98705ba8546c12c3bcd62fe5f
Daniel Luche <daniel.betteloni@gmail.com>
2018-10-13 12:57:45 -0300

    Layout Spot

---
+++ app/src/main/java/com/dluche/motivation/views/SpotActivity.kt
@@ -0,0 +1,13 @@
+package com.dluche.motivation.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dluche.motivation.R
+
+class SpotActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot)
+    }
+}
